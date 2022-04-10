c3a88efb9dd97d5ddb52791bb864578b3cbab378
Matheus Couto <matheuscoutoferreira@hotmail.com>
2022-04-09 21:28:51 -0300

    Implementa o sistema de busca

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -7,14 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poke-list.component.scss'],
 })
 export class PokeListComponent implements OnInit {
+  private setAllPokemons: any;
   public getAllPokemons: any;
 
   constructor(private pokeApiService: PokeApiService) {}
 
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe((res) => {
-      this.getAllPokemons = res.results;
-      console.log(this.getAllPokemons);
+      this.setAllPokemons = res.results;
+      this.getAllPokemons = this.setAllPokemons;
+      //console.log(this.getAllPokemons);
     });
+  }
+
+  public getSearch(value: string) {
+    const filter = this.setAllPokemons.filter((res: any) => {
+      return !res.name.indexOf(value.toLowerCase());
+    });
+
+    this.getAllPokemons = filter;
   }
 }
